ba90ce053b16130c093d7fbba3e8c8b12de28f65
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-02 11:14:53 +0000

    new random number generator

--- gcs.tf
+++ gcs.tf
@@ -4,13 +4,12 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "ai-demo-bucket-3336"
+  name     = "ai-demo-bucket-${random_id.random.hex}"
   location = "EU"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "bucket2" {
-  name     = "ai-demo-bucket-3337"
-  location = "EU"
-  uniform_bucket_level_access = true
+# I want a random number to be generated and used in the bucket name
+resource "random_id" "random" {
+  byte_length = 4
 }
