d485ce75cd718032a00dbcfbe221225db0ba359f
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-05 16:47:22 +0000

    [GFTCodeFix]: Update on  gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -1,15 +1,1 @@
-provider "google" {
-  project = "dev-env-1-412811"
-  region  = "eu-west1"
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = "ai-demo-bucket-${random_id.random.hex}"
-  location = "EU"
-  uniform_bucket_level_access = true
-}
-
-resource "random_id" "random" {
-  byte_length = 3
-}
-
+Erro na chamada à API: {0}Unauthorized
