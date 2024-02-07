38825850b5ae758589d5b431b3335993b3aa41f0
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-07 15:54:59 +0000

    [GFTCodeFix]: Update on  gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -12,15 +12,27 @@ resource "google_storage_bucket" "bucket" {
   name                        = "ai-demo-bucket-${random_id.bucket_id.hex}"
   location                    = "EU"
   uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  logging {
+    log_bucket        = "ai-demo-logs-bucket"
+    log_object_prefix = "log"
+  }
 }
 
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
+resource "google_storage_bucket" "logs_bucket" {
+  name     = "ai-demo-logs-bucket"
+  location = "EU"
+}
+
 resource "google_storage_bucket_object" "object" {
-  name   = "test.html"
-  bucket = google_storage_bucket.bucket.name
-  source = "test.html"
+  name         = "test.html"
+  bucket       = google_storage_bucket.bucket.name
+  source       = "test.html"
   content_type = "text/html"
 }
