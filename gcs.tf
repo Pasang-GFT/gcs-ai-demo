79e81dbdd07ad370888f83f9028a372fb125bad5
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-09 16:23:58 +0000

    new gcs bucket config codefix

--- gcs.tf
+++ gcs.tf
@@ -1,15 +1,22 @@
 provider "google" {
-  project     = "dev-env-1-412811"
-  region      = "europe-west1"
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 8
+  project = "dev-env-1-412811"
+  region  = "europe-west1"
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "ai-demo-${random_id.bucket_id.hex}"
-  location = "europe-west1"
+  name     = "my-bucket"
+  location = "EUROPE-WEST1"
+  project  = "dev-env-1-412811"
 
   uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 7
+    }
+  }
 }
