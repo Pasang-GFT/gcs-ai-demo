a420e7fb3a9ec0bbdb23890381e5f93e980ce706
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-09 15:14:21 +0000

    change gcs config

--- gcs.tf
+++ gcs.tf
@@ -9,14 +9,7 @@ resource "random_id" "bucket_id" {
 
 resource "google_storage_bucket" "bucket" {
   name     = "ai-demo-${random_id.bucket_id.hex}"
+  location = "europe-west1"
 
   uniform_bucket_level_access = true
 }
-
-resource "google_storage_bucket_iam_member" "bucket_iam" {
-  bucket = google_storage_bucket.bucket.name
-  role   = "roles/storage.objectViewer"
-  member = "allUsers"
-}
-```
-
