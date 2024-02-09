c9544a4d0e981325bf5288f539259774ca39b14d
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-09 11:31:35 +0000

    test error

--- gcs.tf
+++ gcs.tf
@@ -9,7 +9,6 @@ resource "random_id" "bucket_id" {
 
 resource "google_storage_bucket" "bucket" {
   name     = "ai-demo-${random_id.bucket_id.hex}"
-  location = "EU"
 
   uniform_bucket_level_access = true
 }
