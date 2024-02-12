26e2da9f607f54635ad357d8481bb20bec86bd27
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-12 11:08:00 +0000

    [GFTCodeFix]: Update on  gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -7,6 +7,15 @@ resource "google_storage_bucket" "bucket" {
   name     = "my-bucket-1672"
   location = "EU"
 
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    log_bucket        = "my-logs-bucket"
+    log_object_prefix = "log"
+  }
+
   uniform_bucket_level_access = true
 
   lifecycle_rule {
