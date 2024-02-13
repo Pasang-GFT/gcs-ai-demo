339c1a7cf39ab2b63ac68d08422f080d04518068
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-13 14:35:41 +0000

    [GFTCodeFix]: Update on  gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -3,9 +3,16 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-1672"
-  location =
+  name                        = "my-bucket-1672"
+  location                    = "US"
   uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  logging {
+    log_bucket        = "my-logs-bucket"
+    log_object_prefix = "log"
+  }
 
   lifecycle_rule {
     action {
@@ -17,4 +24,3 @@ resource "google_storage_bucket" "bucket" {
     }
   }
 }
-#test
