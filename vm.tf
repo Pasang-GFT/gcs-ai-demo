4e81cca9a3b41465e85f3fa8f98209873d8adf4f
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-13 14:06:50 +0000

    [GFTCodeFix]: Update on  vm.tf

--- vm.tf
+++ vm.tf
@@ -3,6 +3,10 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   zone         = "us-central1-a"
 
+  metadata = {
+    block-project-ssh-keys = "true"
+  }
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
@@ -11,9 +15,18 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     network = "default"
-
     access_config {
       // Ephemeral IP
     }
+  }
+
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  // Ensure that the instance is not publicly accessible by omitting the access_config block
+  network_interface {
+    network = "default"
+    // No access_config block specified
   }
 }
