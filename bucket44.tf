14380f63d52aa0a49f0665c1297896a26311b8cc
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-27 10:45:50 +0000

    [GFTCodeFix]: Update on  bucket44.tf

--- bucket44.tf
+++ bucket44.tf
@@ -8,7 +8,13 @@ resource "random_id" "bucket_id" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-${random_id.bucket_id.hex}"
-  location = "US"
+  name                        = "my-bucket-${random_id.bucket_id.hex}"
+  location                    = "US"
+  versioning {
+    enabled = true
+  }
+  logging {
+    log_bucket        = "my-logs-bucket"
+    log_object_prefix = "log"
+  }
 }
-
